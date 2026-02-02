a2ee0dcd3c58aa7c1fafc55542f8d3c2aa607ead
Your Name <147142019+kjc21@users.noreply.github.com>
2026-02-01 21:33:17 -0800

    Addition of SVG Art

--- vis.js
+++ vis.js
@@ -8,10 +8,11 @@ const data = [
     { label: "Saturday", value: 9},
     { label: "Sunday", value: 8}
 ];
+
 // SVG setup
-const svg = document.getElementById("barChart");
-const width = svg.clientWidth;
-const height = svg.clientHeight;
+const chartSvg = document.getElementById("barChart")
+const width = chartSvg.clientWidth;
+const height = chartSvg.clientHeight;
 const padding = 40;
 const barWidth = (width - 2 * padding) / data.length;
 const maxValue = Math.max(...data.map(d => d.value));
@@ -33,7 +34,7 @@ data.forEach((d, i) => {
 
     rect.addEventListener("mouseover", () => {
         tooltip.style.opacity = 1;           // show tooltip
-        tooltip.textContent = `Value: ${d.value}`;
+        tooltip.textContent = `Hours: ${d.value}`;
     });
 
     rect.addEventListener("mousemove", (e) => {
@@ -45,7 +46,7 @@ data.forEach((d, i) => {
         tooltip.style.opacity = 0;           // hide tooltip
     });
 
-    svg.appendChild(rect);
+    chartSvg.appendChild(rect);
 
     // X-axis label
     const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
@@ -54,7 +55,7 @@ data.forEach((d, i) => {
     text.textContent = d.label;
     text.classList.add("label");
 
-    svg.appendChild(text);
+    chartSvg.appendChild(text);
 });
 
 
@@ -64,7 +65,7 @@ xAxisTitle.setAttribute("x", width / 2);
 xAxisTitle.setAttribute("y", height - 5);
 xAxisTitle.textContent = "Days of Week";
 xAxisTitle.classList.add("x-axis-title");
-svg.appendChild(xAxisTitle);
+chartSvg.appendChild(xAxisTitle);
 
 // Y-axis title
 const yAxisTitle = document.createElementNS("http://www.w3.org/2000/svg", "text");
@@ -73,4 +74,30 @@ yAxisTitle.setAttribute("y", 15);
 yAxisTitle.setAttribute("transform", "rotate(-90)");
 yAxisTitle.textContent = "Hours of Sleep";
 yAxisTitle.classList.add("y-axis-title");
-svg.appendChild(yAxisTitle);
+chartSvg.appendChild(yAxisTitle);
+
+
+
+//Art Svg
+const artSvg = document.getElementById("art");
+
+if (artSvg) {
+  const parts = [
+    { cx: 100.5, cy: 100.5, r:40},
+    { cx: 110, cy: 150, r:40},
+    {cx: 155, cy: 125, r:40},
+    { cx: 200, cy: 150, r: 40 },
+    { cx: 150, cy: 150, r: 40 },
+    { cx: 70, cy: 150, r: 40 }
+  ];
+
+  parts.forEach(p => {
+    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.setAttribute("cx", p.cx);
+    circle.setAttribute("cy", p.cy);
+    circle.setAttribute("r", p.r);
+    circle.setAttribute("class", "art-shape");
+
+    artSvg.appendChild(circle);
+  });
+}
