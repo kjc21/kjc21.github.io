6d90d14e05140dcb23983d5b3df730a66e61aa6a
Your Name <147142019+kjc21@users.noreply.github.com>
2026-02-11 18:44:11 -0800

    Added in A3 content from exampleGit

---
+++ a3vis.js
@@ -0,0 +1,38 @@
+// Load data from datasets/videogames_wide.csv using d3.csv and then make visualizations
+async function fetchData() {
+  const data = await d3.csv("./a3dataset/videogames_wide.csv");
+  return data;
+}
+
+fetchData().then(async (data) => {
+  const vlSpec = vl
+    .markBar()
+    .data(data)
+    .encode(
+      vl.y().fieldN("Platform").sort("-x"),
+      vl.x().fieldQ("Global_Sales").aggregate("sum")
+    )
+    .width("container")
+    .height(400)
+    .toSpec();
+
+  const vlSpec2 = vl
+    .markBar()
+    .data(data)
+    .encode(
+      vl.y().fieldN("Genre").sort("-x"),
+      vl.x().fieldQ("Global_Sales").aggregate("sum"),
+      vl.color().value("teal")
+    )
+    .width("container")
+    .height(400)
+    .toSpec();
+
+  render("#view", vlSpec);
+  render("#view2", vlSpec2);
+});
+
+async function render(viewID, spec) {
+  const result = await vegaEmbed(viewID, spec);
+  result.view.run();
+}
