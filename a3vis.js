0aae8594cd98e13f91647d6805d06fd17c94e69f
Your Name <147142019+kjc21@users.noreply.github.com>
2026-02-12 15:38:49 -0800

    Adding Publisher visualizations

--- a3vis.js
+++ a3vis.js
@@ -22,7 +22,7 @@ fetchData().then(async (data) => {
       vl.x().fieldQ("Global_Sales").aggregate("sum")
     )
     .width("container")
-    .height(400)
+    .height(200)
     .toSpec();
 
   const vlSpec2 = vl
@@ -34,7 +34,7 @@ fetchData().then(async (data) => {
       vl.color().value("teal")
     )
     .width("container")
-    .height(400)
+    .height(200)
     .toSpec();
 
     //PLATFORM: Mode - counting which platform has the most releases 
@@ -47,7 +47,7 @@ fetchData().then(async (data) => {
      vl.color().value("orange")
     )
     .width("container")
-    .height(400)
+    .height(300)
     .toSpec();
 
     //PLATFORM: Mean - averaged overall sales per platform
@@ -116,6 +116,103 @@ fetchData().then(async (data) => {
     .toSpec();
 
     //PUBLISHER:Mode - counting publishers, which releases the most games
+    const pubMode = vl
+    .markBar()
+    .data(data)
+    .config(
+      { axis: {
+        titleFont: "Helvetica",
+        titleFontSize: 12,
+        titleFontWeight: "bold",
+        background: "#ffffff"
+      }}
+      )
+    .transform(
+      vl.aggregate([{ op: "count", as: "count" }]).groupby(["Publisher"]),
+      vl.window([{ op: "rank", as: "rank" }]).sort([{ field: "count", order: "descending" }]),
+      vl.filter("datum.rank <= 10")
+    )
+    .encode(
+      vl.y().fieldN("Publisher").sort("-x").title("Publisher").axis({ labelFontSize: 8,labelLimit: 150 }),
+      vl.x().fieldQ("count").title("Number of Games Released"),
+      vl.color().value("#96003e"),
+      vl.tooltip([{ field: "Publisher", type: "nominal", title: "Publisher" },{ field: "count", type: "quantitative", title: "Number of Games" }])
+    )
+    .width("container")
+    .height(300)
+    .title({
+      text:"Top 10 Publishers by Number of Games", 
+      anchor: "middle",
+
+
+    })
+    .toSpec();
+
+    //PUBLISHER:Mean - comparing publishers general average sales - might omit this just for story telling (get rid of one hit wonders)
+     const pubMean = vl
+    .markBar()
+    .data(data)
+    .config(
+      {axis: {
+        labelFont: "Helvetica",
+        labelFontSize: 10,
+        titleFont: "Helvetica",
+        titleFontSize: 14,
+        titleFontWeight: "bold"}}
+      )
+    .transform(
+      vl.aggregate([{ op: "mean", field: "Global_Sales", as: "meanSales" }]).groupby(["Publisher"]),
+
+      vl.window([{ op: "rank", as: "rank" }]).sort([{ field: "meanSales", order: "descending" }]),
+
+      vl.filter("datum.rank <= 10")
+     )
+    .encode(
+
+      vl.y().fieldN("Publisher").sort("-x").title("Publisher"),
+      vl.x().fieldQ("meanSales").title("Mean Global Sales (Millions of Untits)"),
+      vl.tooltip([{ field: "Publisher", type: "nominal", title: "Publisher" },
+      { field: "meanSales", type: "quantitative", title: "Mean Sales (Millions)" }
+      ]),
+      
+      vl.color().value("gray")
+    )
+    .width("container")
+    .height(300)
+    .toSpec();
+    
+    //PUBLISHER:Median - comparing publishers median sales, more stable/ who is the most consistent
+    const pubMedian = vl
+    .markBar()
+    .data(data)
+    .config(
+      {axis: {
+        labelFont: "Helvetica",
+        labelFontSize: 10,
+        titleFont: "Helvetica",
+        titleFontSize: 14,
+        titleFontWeight: "bold"}}
+      )
+    .transform(
+       // calculate median global sales per publisher
+      vl.aggregate([{ op: "median", field: "Global_Sales", as: "medianSales" }]).groupby(["Publisher"]),
+
+      // rank publishers by median sales
+      vl.window([{ op: "rank", as: "rank" }]).sort([{ field: "medianSales", order: "descending" }]),
+
+     // keep only top 10
+      vl.filter("datum.rank <= 10")
+     )
+    .encode(
+      vl.y().fieldN("Publisher").sort("-x"),
+      vl.x().fieldQ("medianSales").title("Median Global Sales (Millions)"),
+      vl.tooltip([{ field: "Publisher", type: "nominal", title: "Publisher" },
+      { field: "medianSales", type: "quantitative", title: "Median Sales (Millions)" }]),
+      vl.color().value("#FF5733")
+    )
+    .width("container")
+    .height(300)
+    .toSpec();
 
   render("#view", vlSpec);
   render("#view2", vlSpec2);
@@ -125,6 +222,9 @@ fetchData().then(async (data) => {
   render("#genreMode", genMode);
   render("#genreMean", genMean);
   render("#genreMedian", genMedian);
+  render("#publisherMode", pubMode);
+  render("#publisherMedian", pubMedian);
+  render("#publisherMean", pubMean);
 });
 
 async function render(viewID, spec) {
